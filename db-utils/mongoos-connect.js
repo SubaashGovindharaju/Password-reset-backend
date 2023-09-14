79f8b6a3a4e54111296fd92bc184e8d03c8f02ce
Subaash G <104092882+SubaashGovindharaju@users.noreply.github.com>
2023-09-14 12:42:38 +0530

    created cloud db

--- db-utils/mongoos-connect.js
+++ db-utils/mongoos-connect.js
@@ -3,10 +3,16 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
+const username = process.env.DB_USERNAME || '';
+const password = process.env.DB_PASSWORD || '';
+const clusterName=process.env.DB_CLUSTER || '';
+const dbName=process.env.DB_NAME || '';
+const cloudMongoUrl=`mongodb+srv://${username}:${password}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+
 const localMongoUrl='mongodb://localhost:27017/day-39-Nodejs'
 const connectToDb = async () => {
     try {
-        await mongoose.connect(localMongoUrl,
+        await mongoose.connect(cloudMongoUrl,
             {
                 useNewUrlParser: true,
             });
