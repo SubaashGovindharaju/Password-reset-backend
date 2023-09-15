b9422fb66ada344ced6b1c18938abe9618e1bd94
Subaash G <104092882+SubaashGovindharaju@users.noreply.github.com>
2023-09-15 13:59:50 +0530

    updated

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use('/api/users',userRouter);
 app.use('/api/auth',authRouter);
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://password-reset-subaash.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 
 app.listen(PORT, () => {
